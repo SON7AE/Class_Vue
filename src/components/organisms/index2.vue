dd12aedd18e950cdc2ef0524364f92637a338d33
SON7AE <bobsot0225@gmail.com>
2023-11-30 09:28:15 +0900

    CompositionAPI 코드로 리뉴얼

--- src/components/organisms/index2.vue
+++ src/components/organisms/index2.vue
@@ -21,69 +21,79 @@
     </div>
 </template>
 
-<script setup>
+<script>
 import WEATHERBOX from "@components/mocules/WeatherBox.vue"
 import GRAPH from "@components/mocules/Graph.vue"
 import DAILYWEATHER from "@components/mocules/DailyWeather.vue"
 import MAP from "@components/mocules/Map.vue"
 import store from "@store/index"
 import dayjs from "dayjs"
-import { reactive, computed } from "vue"
 
-// Default Seoul Axis
-const axis = reactive({
-    lat: 37.5683,
-    lon: 126.9778,
-    cityName: "Seoul",
-})
-store.dispatch("openWeatherApi/fetchApi", axis)
-
-const cityName = computed(() => {
-    // 해당 도시 이름
-    return store.state.openWeatherApi.cityName
-})
-const date = computed(() => {
-    // 현재 날짜 및 시간
-    const current = dayjs(new Date())
-    return current.format("dd, DD MMM YYYY")
-})
-const currentData = computed(() => {
-    // 현재 시간에 따른 날씨 데이터
-    return store.state.openWeatherApi.current
-})
-const graphData = computed(() => {
-    // 그래프 데이터
-    const res = [
-        {
-            label: "Precipitation",
-            value: "38%",
+export default {
+    components: { WEATHERBOX, GRAPH, DAILYWEATHER, MAP },
+    data() {
+        return {
+            axis: {
+                // Default Seoul Axis
+                lat: 37.5683,
+                lon: 126.9778,
+                cityName: "Seoul",
+            },
+        }
+    },
+    created() {
+        store.dispatch("openWeatherApi/fetchApi", this.axis)
+    },
+    computed: {
+        cityName() {
+            // 해당 도시 이름
+            return store.state.openWeatherApi.cityName
         },
-        {
-            label: "Humidity",
-            value: currentData.value.humidity + "%",
+        date() {
+            // 현재 날짜 및 시간
+            const current = dayjs(new Date())
+            return current.format("dd, DD MMM YYYY")
         },
-        {
-            label: "UV Index",
-            value: currentData.value.uvi + "μω/㎠",
+        currentData() {
+            // 현재 시간에 따른 날씨 데이터
+            return store.state.openWeatherApi.current
         },
-    ]
-    return res
-})
-const hourlyData = computed(() => {
-    // 일일 데이터
-    const res = store.state.openWeatherApi.hourly
-    res.forEach((item) => {
-        item.dt = Unix_timeStamp(item.dt)
-    })
+        graphData() {
+            // 그래프 데이터
+            const res = [
+                {
+                    label: "Precipitation",
+                    value: "38%",
+                },
+                {
+                    label: "Humidity",
+                    value: this.currentData.humidity + "%",
+                },
+                {
+                    label: "UV Index",
+                    value: this.currentData.uvi + "μω/㎠",
+                },
+            ]
+            return res
+        },
+        hourlyData() {
+            // 일일 데이터
+            const res = store.state.openWeatherApi.hourly
+            res.forEach((item) => {
+                item.dt = this.Unix_timeStamp(item.dt)
+            })
 
-    return res
-})
-// 시간데이터 양식
-function Unix_timeStamp(dt) {
-    let date = new Date(dt * 1000)
-    let hour = date.getHours().toString().padStart(2, "0")
+            return res
+        },
+    },
+    methods: {
+        Unix_timeStamp(dt) {
+            let date = new Date(dt * 1000)
+            let hour = date.getHours().toString().padStart(2, "0")
 
-    return hour.substring(-2) + "시"
+            return hour.substring(-2) + "시"
+        },
+    },
 }
 </script>
 
